b48bd3cc30c9c94b16161645f26d3a7b520f282b
JafferAliN <mohammedjafferalin98@gmail.com>
2025-12-12 16:56:04 +0530

    Initial commit: Added .NET Core 8 API project

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public EmployeeController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet(Name = "GetEmployee")]
+        public EmployeeInformation GetEmployee()
+        {
+            return new EmployeeInformation { EmployeeId = 1, EmployeeName="John" };
+        }
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
---
+++ Models/EmployeeInformation.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Models
+{
+    public class EmployeeInformation
+    {
+        public string EmployeeName { get; set; }
+        public long EmployeeId { get; set; }
+    }
+}
